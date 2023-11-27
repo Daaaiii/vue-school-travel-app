033c4bcfd8dc61fe3aa88579755cdb76bb062236
Daiane Bolzan <dainha85@gmail.com>
2023-11-27 12:51:06 -0300

    feat: detecting navigation failures

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,12 @@
 <template>
   <div class="home">
     <h1>All destinations</h1>
+    <button @click="triggerRouteError">Trigger Router Error</button>
     <div class="destinations">
       <RouterLink v-for="destination in destinations" :key="destination.id"
         :to="{ name: 'destination.show', params: { id: destination.id, slug: destination.slug } }">
 
-        
+
         <h2>{{ destination.name }}</h2>
         <img :src="`/images/${destination.image}`" :alt="destination.name" />
       </RouterLink>
@@ -20,11 +21,22 @@
 
 
 import sourceData from "@/data.json";
+import { isNavigationFailure, NavigationFailureType } from "vue-router";
 
 export default {
   data() {
     return {
       destinations: sourceData.destinations
+    }
+  }, methods: {
+    async triggerRouteError() {
+      const navigationFailure = await this.$router.push('/')
+      if (isNavigationFailure(navigationFailure, NavigationFailureType.duplicated)) {
+        console.log(navigationFailure.to)
+        console.log(navigationFailure.from)
+      } else {
+
+      }
     }
   }
 }
